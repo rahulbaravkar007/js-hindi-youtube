aa63e7cd2a4fbfb566c3c9f6e0dbcddf9e0d5928
rahulbaravkar007 <164220193+rahulbaravkar007@users.noreply.github.com>
2024-03-24 12:34:41 +0000

    conversion of data type

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 122344
+let accountEmail = " rahul@google.com"
+var accountPassword = "12345"
+accountCity = " Barshi"
+
+accountEmail = " mit@corer.com"
+accountPassword = " 22222"
+accountCity = " Agalgaon "
+let accountState;
+
+// accountId = 3
+
+console.log(accountId);
+console.table([accountId , accountEmail , accountPassword , accountCity ,  accountState] );
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,9 @@
+console.log(" Rahul ")
+
+let name = " baravkar"
+let age = 4
+//let  isLoggedIn = flase 
+let state;
+
+
+console.log(typeof null);
